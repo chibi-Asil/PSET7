ccea8a9fa193f023e86ce911b11c87a123fe1b65
chibi-Asil <32022127+chibi-Asil@users.noreply.github.com>
2020-10-23 19:24:33 -0400

    Create Houses.py

---
+++ Houses.py
@@ -0,0 +1,5 @@
+# This is from a previous coursework and is not with the new and revised coursework. This is for my own edification and to practise what I learn
+# https://cs50.harvard.edu/x/2020/psets/7/houses/
+
+# Implement a program to import student data into a database, and then produce class rosters.
+
